bbc7d6f99799b6b1e81b1f8d199b11b4bfbbb08c
Wolfgang Wagner <someemail>
2016-04-09 20:34:42 -0400

    IP/Port for node

--- template/server.js
+++ template/server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-var ip = process.env.OPENSHIFT_DIY_IP;
-var port = process.env.OPENSHIFT_DIY_PORT;
+var ip = process.env.OPENSHIFT_NGINJS_IP;
+var port = process.env.OPENSHIFT_NGINJS_PORT;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
