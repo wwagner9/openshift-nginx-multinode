814a4595d74e33b49ec4357e160deee3d61bd858
Wolfgang Wagner <someemail>
2016-04-19 22:28:45 -0400

    different port

--- template/server.js
+++ template/server.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 
 var ip = process.env.OPENSHIFT_NGINJS_IP;
-var port = process.env.OPENSHIFT_NGINJS_PORT;
+var port = 10000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(port, ip, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listening on port 10000!');
 });
