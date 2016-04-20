34c3ec4b66c75b5cca3c5688801bd9ac1981bda1
Wolfgang Wagner <someemail>
2016-04-19 23:03:05 -0400

    Testing

--- template/server.js
+++ template/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var ip = process.env.OPENSHIFT_NGINJS_IP;
-var port = 10000;
+var port = 16000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
