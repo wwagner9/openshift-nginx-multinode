d98bef199180ff3781588d12979dbca4d9f76d4b
Wolfgang Wagner <someemail>
2016-04-20 22:54:24 -0400

    port

--- template/server/server.js
+++ template/server/server.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 
 var ip = process.env.OPENSHIFT_NGINJS_IP;
-var port = process.env.OPENSHIFT_NGINJS_IP;
+var port = process.env.OPENSHIFT_NGINJS_PORT;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(port, ip, function () {
-  console.log('Example app listening on port 10000!');
+  console.log('Example app listening on ' + ip + ':' + port);
 });
